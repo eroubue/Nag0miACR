a72906d4a7660f4a3975492fd7cc61bb86ff81d5
Nag0mi <768047653@qq.com>
2026-06-21 13:39:30 +0800

    first commit

---
+++ Gunbreaker/Opener/妖星.cs
@@ -0,0 +1,69 @@
+using Gunbreaker.Data;
+using PromeRotation.Core;
+using PromeRotation.Data;
+using PromeRotation.Extensions;
+using PromeRotation.Rotation;
+
+namespace Gunbreaker.Opener;
+
+public class 妖星 : IOpener
+{
+    public string OpenerName => "妖星起手";
+
+    public List<PAction> InCombatSequence => new()
+    {
+        new PAction(GunbreakerSkill.利刃斩, ActionType.Gcd, ActionTargetType.Target)
+        {
+            RequiresVerification = true
+        },
+        new PAction(GunbreakerSkill.无情, ActionType.OffGcd, ActionTargetType.Self)
+        {
+            RequiresVerification = true
+        },
+        new PAction(GunbreakerSkill.血壤, ActionType.OffGcd, ActionTargetType.Self)
+        {
+            RequiresVerification = true
+        },
+        new PAction(GunbreakerSkill.烈牙, ActionType.Gcd, ActionTargetType.Target)
+        {
+            RequiresVerification = true
+        },
+    };
+
+    public void InitializeCountdown(CountDownHandler countdownHandler)
+    {
+       
+       
+        if (Core.Me.HasStatus(GunbreakerBuff.王室亲卫)&&GunbreakerSettings.Instance.ST&&QT.QTGET(GunbreakerQT.强制盾姿))
+            countdownHandler.AddAction(
+                4500,
+                new PAction(GunbreakerSkill.关盾姿, ActionType.OffGcd, ActionTargetType.Self)
+                {
+                    RequiresVerification = true
+                });
+        
+        if (Core.Me.HasStatus(GunbreakerBuff.王室亲卫)&&!GunbreakerSettings.Instance.ST&&QT.QTGET(GunbreakerQT.强制盾姿))
+            countdownHandler.AddAction(
+                4500,
+                new PAction(GunbreakerSkill.盾姿, ActionType.OffGcd, ActionTargetType.Self)
+                {
+                    RequiresVerification = true
+                });
+        
+        if (QT.QTGET(GunbreakerQT.突进起手))
+
+            countdownHandler.AddAction(
+                300,
+                new PAction(GunbreakerSkill.弹道, ActionType.OffGcd, ActionTargetType.Target)
+                {
+                    RequiresVerification = true
+                });
+        else
+            countdownHandler.AddAction(
+                300,
+                new PAction(GunbreakerSkill.闪雷弹, ActionType.Gcd, ActionTargetType.Target)
+                {
+                    RequiresVerification = true
+                });
+    }
+}
